61c441fe7812956b5cd20ecae51558c93c4a0c3d
nkq232 <khanhquanflash@gmail.com>
2023-11-09 16:56:49 +0700

    add update and delete user

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,18 +2,19 @@ package com.example.demo.service;
 
 import com.example.demo.dto.UserDTO;
 import com.example.demo.entity.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.List;
 
 
 public interface UserService {
     User findActiveUser(String username);
-    int removeUser(String username);
-
-    UserDTO addUser(UserDTO dto);
     List<UserDTO> findAllActive();
-
     List<UserDTO> findAll();
+    UserDTO addUser(UserDTO dto);
+    UserDTO updateUser(ObjectNode objectNode) throws JsonProcessingException;
+    String removeUser(String username);
 
 
 }
